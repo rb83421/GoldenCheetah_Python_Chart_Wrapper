a6b5c7d2d8f03afff2eb0694bb8671e5bca291c7
rb83421 <gamerbeaker@gmail.com>
2020-01-03 09:28:40 +0100

    Initial project start

---
+++ GC_DATA/GC_wrapper.py
@@ -0,0 +1,170 @@
+# Types from GoldenCheetah
+SERIES_SECS = 0
+SERIES_CAD = 1
+SERIES_CADD = 2
+SERIES_HR = 3
+SERIES_HRD = 4
+SERIES_KM = 5
+SERIES_KPH = 6
+SERIES_KPHD = 7
+SERIES_NM = 8
+SERIES_NMD = 9
+SERIES_WATTS = 10
+SERIES_WATTSD = 11
+SERIES_ALT = 12
+SERIES_LON = 13
+SERIES_LAT = 14
+SERIES_HEADWIND = 15
+SERIES_SLOPE = 16
+SERIES_TEMP = 17
+SERIES_INTERVAL = 18
+SERIES_NP = 19
+SERIES_XPOWER = 20
+SERIES_VAM = 21
+SERIES_WATTSKG = 22
+SERIES_LRBALANCE = 23
+SERIES_LTE = 24
+SERIES_RTE = 25
+SERIES_LPS = 26
+SERIES_RPS = 27
+SERIES_APOWER = 28
+SERIES_WPRIME = 29
+SERIES_ATISS = 30
+SERIES_ANTISS = 31
+SERIES_SMO2 = 32
+SERIES_THB = 33
+SERIES_RVERT = 34
+SERIES_RCAD = 35
+SERIES_RCONTACT = 36
+SERIES_GEAR = 37
+SERIES_O2HB = 38
+SERIES_HHB = 39
+SERIES_RPCO = 40
+SERIES_LPPB = 41
+SERIES_RPPB = 42
+SERIES_LPPE = 43
+SERIES_RPPE = 44
+SERIES_LPPPB = 45
+SERIES_RPPPB = 46
+SERIES_LPPPE = 47
+SERIES_RPPPE = 48
+SERIES_WBAL = 49
+SERIES_TCORE = 50
+SERIES_CLENGTH = 51
+SERIES_APOWERKG = 52
+SERIES_INDEX = 53
+SERIES_HRV = 54
+
+def version():  # to get a version string
+    # Not implemented yet
+    return None
+
+
+def build():  # to get a build number
+    # Not implemented yet
+    return None
+
+
+def webpage(filename):  # to set the webpage
+    import webbrowser
+    new = 2  # open in a new tab, if possible
+    webbrowser.open(filename, new=new)
+
+
+# Athlete
+def athlete():  # to get the athlete details
+    # Not implemented yet
+    return None
+
+
+def athleteZones(date=0, sport=""):  # to get zone config
+    # Not implemented yet
+    return None
+
+
+# Activity
+def activities(filter=""):  # to get a list of activities (as dates): #
+    # Not implemented yet
+    return None
+
+
+
+def activity(activity=None):  # to get the activity data
+    # Not implemented yet
+    return None
+
+
+def series(type, activity=None):  # to get an individual series data
+    # Not implemented yet
+    return None
+
+
+def activityWbal(activity=None):  # to get wbal series data    #Not implemented yet
+    # Not implemented yet
+    return None
+
+
+def xdataNames(name="", activity=None):  # to get activity xdata series names
+    # Not implemented yet
+    return None
+
+
+def xdataSeries(name, series, activity=None):  # to get activity xdata series in its own sampling interval
+    # Not implemented yet
+    return None
+
+
+def xdata(name, series, join="repeat", activity=None):  # to get interpolated activity xdata series
+    # Not implemented yet
+    return None
+
+
+def activityMetrics(compare=False):  # to get the activity metrics and metadata
+    from GC_DATA import activity_xxx_metrics
+    return activity_xxx_metrics.activityMetrics
+
+def activityMeanmax(compare=False):  # to get mean maximals for all activity data
+    # Not implemented yet
+    return None
+
+
+def activityIntervals(type="", activity=None):  # to get information about activity intervals
+    # Not implemented yet
+    return None
+
+
+# Trends
+def season(all=False, compare=False):  # to get season details
+    # Not implemented yet
+    return None
+
+
+def seasonMetrics(all=False, filter="", compare=False):  # to get season metrics
+    # Not implemented yet
+    return None
+
+
+def seasonMeanmax(all=False, filter="", compare=False):  # to get best mean maximals for a season
+    # Not implemented yet
+    return None
+
+
+def seasonPeaks(all=False, filter="", compare=False, series="wpk",
+                duration=5):  # to get activity peaks for a given series and duration
+    # Not implemented yet
+    return None
+
+
+def seasonPmc(all=False, metric="TSS"):  # to get PMC data for any given metric
+    # Not implemented yet
+    return None
+
+
+def seasonIntervals(type="", compare=False):  # to get metrics for all intervals
+    # Not implemented yet
+    return None
+
+
+def seasonMeasures(all=False, group="Body"):  # to get Daily measures (Body and Hrv available for v3.5): #
+    # Not implemented yet
+    return None
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Golden Cheetah Python Chart wrapper
+[Golden Cheetah](https://www.goldencheetah.org) (GC) is an analytic software that lets you get the value out of all your data.
+This wrapper program is enabling you to build more advance python charts and debug them.
+
+Within GC you can create python charts for more information on that look [UG_Special Topics_Working with Python](https://github.com/GoldenCheetah/GoldenCheetah/wiki/UG_Special-Topics_Working-with-Python)
+
+This project is created based on GC version 3.5 
+
+
+# Choose your favorite python IDE.
+For this project I use PyCharm for download see [PyCharm](https://www.jetbrains.com/pycharm/).
+Once you have create a project and selected your desired python interpreter for easy interoperability use the same that is configured in GC.
+
+![pycharm setup](https://github.com/rb83421/GoldenCheetah_Python_Chart_Wrapper/imgs/pycharm_setup.png "pycharm setup")
