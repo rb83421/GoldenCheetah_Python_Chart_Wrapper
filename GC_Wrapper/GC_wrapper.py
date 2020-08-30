c2b6233ec656abba6f527dc5adf41ebef11383d6
rb83421 <gamerbeaker@gmail.com>
2020-08-30 10:17:13 +0200

    placeholder for body measurements

--- GC_Wrapper/GC_wrapper.py
+++ GC_Wrapper/GC_wrapper.py
@@ -10,6 +10,7 @@ from GC_DATA import trend_single_extract_all_season_metrics
 from GC_DATA import trend_single_extract_all_tss_pmc
 from GC_DATA import trend_season_peaks_power
 from GC_DATA import trend_season_peaks_wpk
+from GC_DATA import trend_season_measures
 
 from GC_DATA import trend_season_compare_peaks_power
 from GC_DATA import trend_season_compare_peaks_wpk
@@ -293,5 +294,11 @@ def seasonIntervals(type="", compare=False):  # to get metrics for all intervals
 
 
 def seasonMeasures(all=False, group="Body"):  # to get Daily measures (Body and Hrv available for v3.5): #
-    # Not implemented yet
+    # this can only be turned on when the extract also contains body measurement file: athlete body data
+
+    # import GC_DATA.athlete_body_data
+    # if all and group == "Body":
+    #     return athlete_data.athlete_data
+
     return None
+
